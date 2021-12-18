6e8f3d454caf04ed699e4af5f951d684e5246a17
rommina <romminna.rf2@gmail.com>
2021-12-18 19:26:56 -0300

    heroku

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
+
+
+require("dotenv").config();
 const express = require ("express");
 const mongoose = require("mongoose");
 const bodyParser = require ("body-parser");
 const cors= require ("cors");
 
-const PORT= 3000;
-const MONGO_URL= "mongodb+srv://romina:ROMINA@cluster0.dtszr.mongodb.net/labonline?retryWrites=true&w=majority";
+const PORT= process.env.PORT || 5000;
+const MONGO_URL=process.env.MONGO_DB_URL;
 
 
 const app= express();
 const router= require ("./src/routes/index");
+
+
 
 app.use (bodyParser.json());
 app.use (bodyParser.urlencoded({extended: true}));
